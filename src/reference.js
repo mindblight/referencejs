751743115cbd99d18a3476239b6fd037c431004a
Arion Sprague <me@arionsprague.com>
2017-03-28 13:03:08 -0700

    Added flow, dereference, and smartDereference

--- src/reference.js
+++ src/reference.js
@@ -1,10 +1,24 @@
+// @flow
 import isArray from 'lodash/isArray';
 import isString from 'lodash/isString';
 import isNill from 'lodash/isNil';
+import isPlainObject from 'lodash/isPlainObject';
 import set from 'lodash/set';
-import cloneDeep from 'lodash/cloneDeep'
+import get from 'lodash/get';
+import has from 'lodash/has';
+import cloneDeep from 'lodash/cloneDeep';
+import mapValues from 'lodash/mapValues';
 
-export function isPath(path) {
+
+/* Types */
+export type Path = string[];
+export type Reference = {
+  path :Path,
+};
+export type Store = { [key :string] :* };
+
+/* Validation */
+export function isPath(path :Path) :boolean {
   return isArray(path) &&
     path.length > 0 &&
     path.every((pathPart) => {
@@ -12,19 +26,21 @@ export function isPath(path) {
     });
 }
 
-export function isReference(reference) {
-    return !!reference && isPath(reference.path);
+export function isReference(reference :*) :boolean {
+  return !!reference && isPath(reference.path);
 }
 
-export function createReference(path) {
+/* Reference */
+export function createReference(path :Path) {
   if (!isPath(path)) {
-    throw Error(`path" must be a non-empty array of strings. Received ${path}`);
+
+    throw Error(`path" must be a non-empty array of strings. Received ${path.join(', ')}`);
   }
 
   return { path };
 }
 
-export function resolveReference(store, reference, value) {
+export function resolveReference(store :Store, reference :Reference, value :*) {
   if (isNill(store)) {
     throw new Error('"store" must be defined');
   }
@@ -35,4 +51,48 @@ export function resolveReference(store, reference, value) {
   const newStore = cloneDeep(store);
   set(newStore, reference.path, value);
   return newStore;
+}
+
+export function isValueAtReference(store :Store, reference :Reference) :boolean {
+  if (isNill(store)) {
+    throw new Error('"store" must be defined');
+  }
+  if (!isReference(reference)) {
+    throw new Error('"reference" must be a valid reference');
+  }
+
+  return has(store, reference.path);
+}
+
+/* Dereference */
+export const EMPTY_REFERENCE = Symbol('empty reference');
+export function dereference(store :Store, reference :Reference) :* {
+  if (isNill(store)) {
+    throw new Error('"store" must be defined');
+  }
+  if (!isReference(reference)) {
+    throw new Error('"reference" must be a valid reference');
+  }
+
+  if (isValueAtReference(store, reference)) {
+    return get(store, reference.path);
+  }
+  return EMPTY_REFERENCE;
+}
+
+export function smartDereference(store :Store, val :*) :* {
+  if (isNill(store)) {
+    throw new Error('"store" must be defined');
+  }
+
+  if (isReference(val)) {
+    return dereference(store, val);
+  } else if(isArray(val)) {
+    return val.map(value => smartDereference(store, value));
+  } else if (isPlainObject(val)) {
+    return mapValues(val, (value) => {
+      return smartDereference(store, value);
+    });
+  }
+  return val;
 }
--- test/reference.test.js
+++ test/reference.test.js
@@ -3,9 +3,13 @@ import {
   isPath,
   isReference,
   createReference,
-  resolveReference
+  resolveReference,
+  dereference,
+  EMPTY_REFERENCE,
+  isValueAtReference,
+  smartDereference,
 } from '../src/reference';
-import cloneDeep from 'lodash/cloneDeep'
+import cloneDeep from 'lodash/cloneDeep';
 
 
 describe('reference', function() {
@@ -49,13 +53,14 @@ describe('reference', function() {
     });
   });
 
+
   describe('createReference', function() {
     it('should throw error on null path', function() {
-      expect(createReference.bind(null, null)).to.throw(Error);
+      expect(isValueAtReference.bind(null, null)).to.throw(Error);
     });
 
     it('should throw error on invalid path', function() {
-      expect(createReference.bind(null, [3])).to.throw(Error);
+      expect(isValueAtReference.bind(null, [3])).to.throw(Error);
     });
 
     it('should return valid reference', function() {
@@ -97,11 +102,148 @@ describe('reference', function() {
       const value = 5;
       const initialState = {
         c: 6
-      }
+      };
       const state = resolveReference(initialState, reference, value);
 
       expect(state).to.have.property('c').equal(6);
       expect(state).to.have.deep.property(path.join('.')).equal(value);
+    });
+  });
+
+
+  describe('isValueAtReference', function() {
+    it('should throw error on empty store', function() {
+      expect(dereference.bind(null, null)).to.throw(Error);
+    });
+
+    it('should throw error on invalid reference', function() {
+      expect(dereference.bind(null, {}, {})).to.throw(Error);
+    });
+
+    it('should return false when reference does not exist', function() {
+      const reference = createReference(['a'])
+      expect(isValueAtReference({}, reference)).to.be.false;
+    });
+
+    it('should return value at reference', function() {
+      const value = 9;
+      const store = {
+        a: {
+          b: value
+        }
+      };
+      const reference = createReference(['a', 'b']);
+
+      expect(isValueAtReference(store, reference)).to.be.true;
+    });
+  });
+
+
+  describe('dereference', function() {
+    it('should throw error on empty store', function() {
+      expect(dereference.bind(null, null)).to.throw(Error);
+    });
+
+    it('should throw error on invalid reference', function() {
+      expect(dereference.bind(null, {}, {})).to.throw(Error);
+    });
+
+    it('should return EMPTY_REFERENCE when reference does not exist', function() {
+      expect(dereference({}, createReference(['a']))).to.equal(EMPTY_REFERENCE);
+    });
+
+    it('should return value at reference', function() {
+      const value = 9;
+      const store = {
+        a: {
+          b: value,
+        },
+      };
+      const reference = createReference(['a', 'b']);
+
+      expect(dereference(store, reference)).to.equal(value);
+    });
+  });
+
+
+  describe('smartDereference', function() {
+    const value1 = 9;
+    const value2 = 20;
+    const store = {
+      a: {
+        b: value1,
+        c: value2,
+      },
+    };
+    const reference1 = createReference(['a', 'b']);
+    const reference2 = createReference(['a', 'c']);
+
+    it('should throw error on null store', function() {
+      expect(smartDereference.bind(null, null)).to.throw(Error);
+    });
+
+    it('should return EMPTY_REFERENCE when reference does not exist', function() {
+      expect(smartDereference({}, createReference(['a']))).to.equal(EMPTY_REFERENCE);
+    });
+
+    it('should return value for non-reference input', function() {
+      const value = Symbol();
+      expect(smartDereference(store, value)).to.equal(value);
+    });
+
+    it('should return value at reference', function() {
+      expect(smartDereference(store, reference1)).to.equal(value1);
+    });
+
+    it('should return reference in array', function() {
+      const input = [reference1, reference2];
+      const expected = [value1, value2];
+
+      expect(smartDereference(store, input)).to.deep.equal(expected);
+    });
+
+    it('should ignore non-reference in array', function() {
+      const input = [reference1, value2];
+      const expected = [value1, value2]
+
+      expect(smartDereference(store, input)).to.deep.equal(expected);
+    });
+
+    it('should deeply handle arrays', function() {
+      const input = [reference1, [reference2]];
+      const expected = [value1, [value2]];
+
+      expect(smartDereference(store, input)).to.deep.equal(expected);
+    });
+
+    it('should handle plain objects', function() {
+      const input = {
+        foo: reference1,
+        bar: reference2,
+      };
+      const expected = {
+        foo: value1,
+        bar: value2,
+      };
+
+      expect(smartDereference(store, input)).to.deep.equal(expected);
+    });
+
+    it('should deep handle plain objects', function() {
+      const input = {
+        foo: reference1,
+        bar: {
+          baz: reference2,
+        },
+      };
+      const expected = {
+        foo: value1,
+        bar: {
+          baz: value2,
+        },
+      };
+
+      expect(smartDereference(store, input)).to.deep.equal(expected);
     });
   });
 });
